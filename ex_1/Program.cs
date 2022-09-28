cf1409d71a49b12c3490e33c0fdf2161d105044f
Best Cat <kotova-irina11@rambler.ru>
2022-09-28 22:00:52 +0300

    задача 64 чётные числа рекурия

--- ex_1/Program.cs
+++ ex_1/Program.cs
@@ -8,7 +8,7 @@ int a = int.Parse(Console.ReadLine());
 string Num(int a)
 {
  if( a == 0 ) return " ";
- else if( a % 2 == 0) return $"{Num(a-1) + a}, ";
+ else if( a % 2 == 0) return $"{Num(a-1) + a} \b ";
  else return Num(a-1);
 }
 Console.Write(Num(a)); 
