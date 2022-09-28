d382d4fb825766dd142344b63367e8058b4bf594
Best Cat <kotova-irina11@rambler.ru>
2022-09-28 21:18:47 +0300

    задача 64 чётные числа рекурия

---
+++ ex_1/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задача 64: Задайте значение N. 
+//Напишите программу, которая выведет все чётные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+//N = 5 -> "4, 2"
+//N = 8 -> "8, 6, 4, 2"
+Console.Clear();
+Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+string Num(int a)
+{
+ if( a == 1 ) return "1";
+ else if( a % 2 == 0) return Num(a-1) + a;
+ else return Num(a-1);
+}
+Console.WriteLine(Num(a)); 
