3965668dce3e71a0437da425cd8ba5d1b32e0b32
Best Cat <kotova-irina11@rambler.ru>
2022-09-28 22:29:31 +0300

    задача 66 сумма натуральных элементов  рекурия

---
+++ ex_2/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+//M = 1; N = 15 -> 120
+//M = 4; N = 8. -> 30
+Console.Clear();
+Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+int NaturalNumSum(int n, int a)
+{
+ if( n == a ) return a; 
+ else return n + NaturalNumSum((n - 1), a);
+}
+Console.WriteLine(NaturalNumSum(b, a));
